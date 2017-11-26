95c6eeab78a3b75226ab9f5e7c484d342833ab92
Andrew Heekin <andrewheekin@gmail.com>
2017-11-26 17:36:58 -0500

    advance the deck

--- src/Deck.js
+++ src/Deck.js
@@ -3,13 +3,13 @@ import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
-const SWIPE_OUT_DURATION = 250
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
   static defaultProps = {
     onSwipeRight: () => {},
-    onSwipeLeft: () => {},    
-  }
+    onSwipeLeft: () => {},
+  };
 
   constructor(props) {
     super(props);
@@ -29,23 +29,23 @@ class Deck extends Component {
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
           this.forceSwipe('left');
         } else {
-          this.resetPosition();          
+          this.resetPosition();
         }
-      }, 
+      },
     });
     this.state = { panResponder, position, index: 0 };
   }
 
   resetPosition() {
     Animated.spring(this.state.position, {
-      toValue: { x: 0, y: 0 }
+      toValue: { x: 0, y: 0 },
     }).start();
   }
 
   forceSwipe(direction) {
     const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
     Animated.timing(this.state.position, {
-      toValue: { x, y: 0 }, 
+      toValue: { x, y: 0 },
       duration: SWIPE_OUT_DURATION,
     }).start(() => this.onSwipeComplete(direction));
   }
@@ -57,24 +57,28 @@ class Deck extends Component {
     direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
     this.state.position.setValue({ x: 0, y: 0 });
     this.setState({ index: this.state.index + 1 });
-  };
+  }
 
   getCardStyle = () => {
     const { position } = this.state;
     const rotate = position.x.interpolate({
       inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
-      outputRange: ['-120deg', '0deg', '120deg']
+      outputRange: ['-120deg', '0deg', '120deg'],
     });
 
     return {
       ...this.state.position.getLayout(),
-      transform: [{ rotate }]  // how to rotate a card
+      transform: [{ rotate }], // how to rotate a card
     };
-  }
+  };
 
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    return this.props.data.map((item, idx) => {
+      if (idx < this.state.index) {
+        return null;
+      }
+
+      if (idx === this.state.index) {
         return (
           <Animated.View key={item.id} {...this.state.panResponder.panHandlers} style={this.getCardStyle()}>
             {this.props.renderCard(item)}
