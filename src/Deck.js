d533b7159da0d8fa9e2ea1082e844098d49c4a93
Andrew Heekin <andrewheekin@gmail.com>
2017-11-26 16:41:37 -0500

    animate card exit when swipe threshold is reached

--- src/Deck.js
+++ src/Deck.js
@@ -3,6 +3,7 @@ import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
   constructor(props) {
@@ -19,9 +20,9 @@ class Deck extends Component {
       onPanResponderRelease: (event, gesture) => {
         // when user lifts finger off screen
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swipe right!');
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left!');
+          this.forceSwipe('left');
         } else {
           this.resetPosition();          
         }
@@ -33,6 +34,14 @@ class Deck extends Component {
   resetPosition() {
     Animated.spring(this.state.position, {
       toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 }, 
+      duration: SWIPE_OUT_DURATION,
     }).start();
   }
 
