9dc53e1237915f7af52a3e4b129318c7aa4a4209
Andrew Heekin <andrewheekin@gmail.com>
2017-11-26 17:25:08 -0500

    add onswipecomplete and defaultprops

--- src/Deck.js
+++ src/Deck.js
@@ -6,6 +6,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {},    
+  }
+
   constructor(props) {
     super(props);
 
@@ -28,7 +33,7 @@ class Deck extends Component {
         }
       }, 
     });
-    this.state = { panResponder, position };
+    this.state = { panResponder, position, index: 0 };
   }
 
   resetPosition() {
@@ -42,8 +47,17 @@ class Deck extends Component {
     Animated.timing(this.state.position, {
       toValue: { x, y: 0 }, 
       duration: SWIPE_OUT_DURATION,
-    }).start();
+    }).start(() => this.onSwipeComplete(direction));
   }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
+  };
 
   getCardStyle = () => {
     const { position } = this.state;
